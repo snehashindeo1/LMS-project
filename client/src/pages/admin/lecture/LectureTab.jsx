46fd778544ab8256549496b147dffd47e73a2728
sneha shinde <shindesneha326@gmail.com>
2025-03-21 08:53:27 +0530

    updated frontend build and API urls -2

--- client/src/pages/admin/lecture/LectureTab.jsx
+++ client/src/pages/admin/lecture/LectureTab.jsx
@@ -17,7 +17,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { toast } from "sonner";
 
-const MEDIA_API = "http://localhost:8080/api/v1/media";
+const MEDIA_API = `${import.meta.env.VITE_API_URL}/media`;
 
 const LectureTab = () => {
   const [lectureTitle, setLectureTitle] = useState("");
